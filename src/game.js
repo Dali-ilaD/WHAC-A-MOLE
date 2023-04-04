dd32e9f240240b6db0b4439f26443d153d96ea78
David Lai <pf.davidlai@gmail.com>
2023-04-04 15:19:06 -0700

    added game logic

--- src/game.js
+++ src/game.js
@@ -1,14 +1,65 @@
+
+
+
 class Game {
 
     constructor(){
         this.hiddenTextInput = '';
+        this.mole = '';
+        this.lives = 5;
     }
 
     static VERT = 1000;
     static HORI = 1000;
 
+    generateRandomLetter() {
+        var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+        var letter = letters[Math.floor(Math.random() * letters.length)];
+        return letter
+    }
+     makeMole(randomletter) {
+       
+        const hole = document.getElementById(randomletter).firstElementChild
+        
+        hole.classList.add('pop_up_mole')
 
+        setTimeout(()=>{
+            console.log(hole)
+            hole.classList.remove('pop_up_mole')  
+            this.lives -= 1;
+        }, 2000)
+       
+    }
+
+    whaccMole(key){
+             
+         
+          const hole = document.getElementById(key).firstElementChild
+          console.log(hole, key,hole.classList.contains('pop_up_mole'))
+
+          if(hole.classList.contains('pop_up_mole')){
+            hole.classList.remove('pop_up_mole')
+            hole.classList.add('whacced_mole')
+            setTimeout(() => {hole.classList.remove('whacced_mole')}, 500)
+          }
+    }
+
+    start() {
+        const start = document.getElementById('startbutton')
+        start.addEventListener('click', () =>{
+            setInterval(() => {this.makeMole(this.generateRandomLetter())}, 1000)
+            
+            document.body.addEventListener('keydown',(e) =>{
+            this.whaccMole(e.key)
+
+        })
+    })
+    }
+
+    
 }
+
+
 
 
 export default Game;
--- src/index.js
+++ src/index.js
@@ -1,18 +1,21 @@
 import Game from "./game.js";
 import GameView from "./game_view.js";
 
+
 document.addEventListener("DOMContentLoaded", () => {
-  const canvasEl = document.getElementById("canvas");
-  canvasEl.width = Game.HORI;
-  canvasEl.height = Game.VERT;
+//   const canvasEl = document.getElementById("canvas");
+//   canvasEl.width = Game.HORI;
+//   canvasEl.height = Game.VERT;
   
-  const ctx = canvasEl.getContext("2d");
+//   const ctx = canvasEl.getContext("2d");
   const game = new Game();
+  game.start()
 //   new GameView(game, ctx).start();
-    const img = new Image()
-    img.src = '/Users/dali/Desktop/App Academy Projects/WHAC-A-MOLE/grassygrass.png';
-    img.onload = () => {
-        ctx.drawImage(img,0,0)
-    }
+    // const img = new Image()
+    // img.src = '/Users/dali/Desktop/App Academy Projects/WHAC-A-MOLE/grassygrass.png';
+    // img.onload = () => {
+    //     ctx.drawImage(img,0,0)
+    // }
+    
   
 });
